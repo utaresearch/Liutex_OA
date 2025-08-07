1c6c868465efeb11b09608ab8fca6e602d6693d9
Oscar Alvarez <oscar.alvarez@uta.edu>
2025-08-07 16:14:39 -0500

    Update main.cpp
    added omega liutex test in main

--- Liutex_UTA/Liutex_C++/main.cpp
+++ Liutex_UTA/Liutex_C++/main.cpp
@@ -37,6 +37,7 @@ int main()
     liutex(imag_eigvals_vgt, R_imag, r_imag);
 
     /// Printing Results:
+    std::cout << "\n\n[[ LIUTEX RESULTS ]]" << std::endl;
     std::cout << "\n[ Velocity gradient tensor with all real eigenvalues ]\n";
     std::cout << "Liutex magnitude R: " << R_real << "\n";
     std::cout << "Liutex vector r:  ";
@@ -54,6 +55,46 @@ int main()
     }
 
     std::cout << "\n";
+
+
+    /* EXAMPLE ON HOW TO USE OMEGA LIUTEX */
+
+    // Data Dimensions:
+    int imax = 1;
+    int jmax = 1;
+    int kmax = 1;
+    
+    // Velocity gradient tensor dimensions (imax,jmax,kmax, 3,3)
+    std::vector<std::vector<std::vector<std::vector<std::vector<double>>>>> real_vgt(imax, std::vector<std::vector<std::vector<std::vector<double>>>>
+                                                                                    (jmax, std::vector<std::vector<std::vector<double>>>
+                                                                                    (kmax, std::vector<std::vector<double>>
+                                                                                    (3, std::vector<double>
+                                                                                    (3, 0)))));
+    real_vgt[0][0][0] = { {-2.0, -4.0, 2.0},
+                          {-2.0,  1.0, 2.0},
+                          { 4.0,  2.0, 5.0} };
+
+    std::vector<std::vector<std::vector<std::vector<std::vector<double>>>>> imag_vgt(imax, std::vector<std::vector<std::vector<std::vector<double>>>>
+                                                                                    (jmax, std::vector<std::vector<std::vector<double>>>
+                                                                                    (kmax, std::vector<std::vector<double>>
+                                                                                    (3, std::vector<double>
+                                                                                    (3, 0)))));
+    imag_vgt[0][0][0] = { {4.0, -3.0, 7.0},
+                          { 3.0,  4.0, 0.0 },
+                          { 5.0, 10.0, 10.0} };
+
+    // Initialize/allocate omega_liutex
+    std::vector<std::vector<std::vector<double>>> o_liutex(imax, std::vector<std::vector<double>>(jmax, std::vector<double>(kmax, 0)));
+
+    omega_liutex(real_vgt, o_liutex, imax, jmax, kmax);
+
+    std::cout << "\n\n[[ OMEGA LIUTEX RESULTS ]]" << std::endl;
+    std::cout << "\n[ Velocity gradient tensor with all real eigenvalues ]\n";
+    std::cout << "Real Eigenvalued VGT Omega Liutex: " << o_liutex[0][0][0] << std::endl;
+
+    omega_liutex(imag_vgt, o_liutex, imax, jmax, kmax);
+    std::cout << "\n\n[ Velocity gradient tensor with complex conjugate eigenvalues ]\n";
+    std::cout << "Imaginary Eigenvalued VGT Omega Liutex: " << o_liutex[0][0][0] << std::endl;
 
     return 0;
 
